d1fbdfacada0303533a7a5bc0446ca1eb5c629d5
Gabriel Panza <gabrieljpanza@gmail.com>
2025-01-22 23:57:45 -0300

    best result so far

--- New Data/SalvarFatiasTodas.py
+++ New Data/SalvarFatiasTodas.py
@@ -23,37 +23,6 @@ def calculate_label(subimage, threshold=0.05):
     else:
         return "label0"
 
-# Verifica se um pedaço é o único com pixels `1` e ajusta se necessário
-def adjust_unique_lesion_pieces_with_neighbors(subimages, current_index, total_slices, lesion_data):
-    """
-    Ajusta os pedaços da máscara para garantir que nenhum pedaço seja o único com pixels `1`
-    ao analisar os slices anteriores e posteriores.
-    :param subimages: Lista de subimagens (máscaras divididas em 8 pedaços) do slice atual.
-    :param current_index: Índice do slice atual.
-    :param total_slices: Número total de slices disponíveis.
-    :param lesion_data: Dados completos da máscara de lesão.
-    :return: Lista de subimagens ajustadas.
-    """
-    # Contar o número de pedaços com pixels `1` no slice atual
-    has_lesion_current = [np.any(piece == 1) for piece, _ in subimages]
-
-    # Verificar slices anteriores e posteriores
-    has_lesion_before = 0
-    has_lesion_after = 0
-    if current_index > 0:  # Slice anterior
-        has_lesion_before = [np.any(lesion_data[:, :, current_index - 1] == 1)]
-    if current_index < total_slices - 1:  # Slice posterior
-        has_lesion_after = [np.any(lesion_data[:, :, current_index + 1] == 1)]
-
-    # Atualizar peças únicas com pixels `1` no slice atual
-    total_lesion_pieces_current = sum(has_lesion_current) + has_lesion_before + has_lesion_after
-
-    if sum(has_lesion_current) == 1 and total_lesion_pieces_current <= 1: # Se essa condição for satisfeita então o recorte atual com label 1 é um recorte isolado (sem lesão em cima e em baixo dele)
-        unique_piece_index = has_lesion_current.index(True)
-        subimages[unique_piece_index] = (np.zeros_like(subimages[unique_piece_index][0]), subimages[unique_piece_index][1]) # Zero a mascara lesionada isolada
-
-    return subimages
-
 imagens = "Patients_Displasya"
 mascara = "Mascaras"
 
@@ -96,22 +65,30 @@ for img, mask in zip([f for f in os.listdir(imagens) if f.endswith(('.nii', '.ni
         # Pega a fatia axial atual
         slice_data = data[16:233-17, 18:197-19, slice_idx]            
 
-        output_dir_slice = os.path.join(output_dir, f"Slice{slice_idx}.nii.gz")
+        # Total de pixels na subimagem
+        total_pixels = slice_data.size
+        # Número de pixels não-preto
+        non_zero_pixels = np.count_nonzero(slice_data)
+        # Proporção de pixels não-preto
+        non_black_ratio = non_zero_pixels / total_pixels if total_pixels > 0 else 0
         
-        processed_slices += 1
+        if non_black_ratio >= 0.08:
+            output_dir_slice = os.path.join(output_dir, f"Slice{slice_idx}.nii.gz")
+            
+            processed_slices += 1
+            
+            # Converter o array numpy para um objeto NIfTI
+            subimage_nii = nib.Nifti1Image(lesion_slice_data, affine=np.eye(4))
+            
+            # Salvar o arquivo NIfTI
+            if (lesion_slice_data.size>0 and lesion_slice_data is not None):
+                nib.save(subimage_nii, output_dir_lesion_slice)
 
-        # Converter o array numpy para um objeto NIfTI
-        subimage_nii = nib.Nifti1Image(lesion_slice_data, affine=np.eye(4))
-        
-        # Salvar o arquivo NIfTI
-        if (lesion_slice_data.size>0 and lesion_slice_data is not None):
-            nib.save(subimage_nii, output_dir_lesion_slice)
-
-        # Converter o array numpy para um objeto NIfTI
-        subimage_nii = nib.Nifti1Image(slice_data, affine=np.eye(4))
-        
-        # Salvar o arquivo NIfTI
-        if (slice_data.size>0 and slice_data is not None):
-            nib.save(subimage_nii, output_dir_slice)
+            # Converter o array numpy para um objeto NIfTI
+            subimage_nii = nib.Nifti1Image(slice_data, affine=np.eye(4))
+            
+            # Salvar o arquivo NIfTI
+            if (slice_data.size>0 and slice_data is not None):
+                nib.save(subimage_nii, output_dir_slice)
     
     print(f"Total de fatias processadas do paciente {img.split('_')[0]}: {processed_slices}")
